e6ffa9aa871cd818b5628689cf8809d14dd074f5
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-10-29 08:59:53 +0200

    Create day 19 - part 1 & 2.py

---
+++ day 19 - part 1 & 2.py
@@ -0,0 +1,21 @@
+from math import log, floor
+
+data = 3017957
+linked = {x : x+1 for x in range(1, data)}
+linked[data] = 1
+current = 1
+while len(linked) > 1:
+    elim = linked[current]
+    linked[current] = linked[linked[current]]
+    linked.pop(elim)
+    current = linked[current]
+
+print(current)
+
+def calc(data):
+    x = floor(log(data, 3))
+    y = data - 3**x
+    ans = data if y == 0 else (y if x == 1 or y <= (3**x) else 3**x + 2 * (y-3**l))
+    return ans
+
+print(calc(data))
