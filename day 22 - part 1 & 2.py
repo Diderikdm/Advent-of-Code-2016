a7eb626ec8dc6f9e7af94b1365e9c9210c00a510
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-10-31 21:42:09 +0100

    Update day 22 - part 1 & 2.py

--- day 22 - part 1 & 2.py
+++ day 22 - part 1 & 2.py
@@ -16,7 +16,7 @@ with open("C:\\Advent\\2016\\day22.txt", 'r') as file:
             if k != k2 and 0 < v['Used'] <= v2['Avail']:
                 matches.add((k,k2))
     print(len(matches))
-    grid = [['_' if data[(x,y)]['Use'] < 10 else ('#' if data[(x,y)]['Use'] > 85 else '.') for x in range(sorted(data)[-1][0] + 1)] for y in range(sorted(data, key = lambda z: z[1])[-1][1] + 1)]
+    grid = [[x for x in range(sorted(data)[-1][0] + 1)] for y in range(sorted(data, key = lambda z: z[1])[-1][1] + 1)]
     start = next(iter(k for k,v in data.items() if v['Use'] < 10), None)
     goal = (len(grid[0])-1, 0)
     adj = lambda x,y : list(set([(x+a,y+b) for a,b in list(combinations([-1,0,1]*2, 2)) if sum([a,b]) in [-1,1] and len(grid[0]) > x+a >= 0 and len(grid) > y+b >= 0 and data[(x+a, y+b)]['Use'] <= 85]))
